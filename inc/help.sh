2fa467a8604b7ec01fee4335b7a771721f5e7f76
Feodor Rusanov <f.rusanov@gmail.com>
2021-06-04 00:34:31 +0300

    🐛 Change incorret brackets

--- inc/help.sh
+++ inc/help.sh
@@ -2,10 +2,10 @@ function no_command_such () {
   echo -e "There is no such command as $(bold $1)!"
 }
 
-function base_help (
+function base_help {
   echo -e "Avaible commands:
 
   create - creates $(bold $CONTAINER_NAME) container
   remove - removes $(bold $CONTAINER_NAME) container
     "
-)
+}
