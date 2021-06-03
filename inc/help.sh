d19550e4dffcbbeb52f82253f89849386656536d
Feodor Rusanov <f.rusanov@gmail.com>
2021-06-04 00:28:52 +0300

    🐛 Some fixes

--- inc/help.sh
+++ inc/help.sh
@@ -1,7 +1,12 @@
-echo -e "There is no such command as $(bold $1)!"
-echo -e "
+function no_command_such () {
+  echo -e "There is no such command as $(bold $1)!"
+}
+
+function base_help (
+  echo -e "
 Avaible commands:
 
   create - creates $(bold $CONTAINER_NAME) container
   remove - removes $(bold $CONTAINER_NAME) container
     "
+)
